6d135a2ef9ddfcb86d83b68d6f1d109f673f3e0b
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-03-01 22:45:45 -0300

    Añade grid, posicion div

--- docs/data/DB_Saldo.sql
+++ docs/data/DB_Saldo.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE  IF NOT EXISTS `db_saldo` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `db_saldo`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_saldo
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.14-MariaDB
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,19 +21,19 @@ USE `db_saldo`;
 
 DROP TABLE IF EXISTS `coeficiente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `coeficiente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `mediosdepagos1` int(11) NOT NULL,
-  `mediosdepagos2` int(11) NOT NULL,
-  `coeficiente` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mediosdepagos1` int NOT NULL,
+  `mediosdepagos2` int NOT NULL,
+  `coeficiente` int NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `id_nombre_idx` (`mediosdepagos1`),
   KEY `id_nombre2_idx` (`mediosdepagos2`),
   CONSTRAINT `id_nombre` FOREIGN KEY (`mediosdepagos1`) REFERENCES `mediosdepagos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `id_nombre2` FOREIGN KEY (`mediosdepagos2`) REFERENCES `mediosdepagos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +42,7 @@ CREATE TABLE `coeficiente` (
 
 LOCK TABLES `coeficiente` WRITE;
 /*!40000 ALTER TABLE `coeficiente` DISABLE KEYS */;
-INSERT INTO `coeficiente` VALUES (1,1,2,3),(2,1,3,3),(3,1,4,3),(4,1,5,3),(5,1,6,3),(6,1,7,3),(7,1,8,3),(8,2,1,3),(9,2,3,3),(10,2,4,3),(11,2,5,3),(12,2,6,3),(13,2,7,3),(14,2,8,3),(15,3,1,3),(16,3,2,3),(17,3,4,3),(18,3,5,3),(19,3,6,3),(20,3,7,3),(21,3,8,3);
+INSERT INTO `coeficiente` VALUES (1,1,2,2),(2,1,3,2),(3,1,4,2),(4,2,1,2),(5,2,3,2),(6,2,4,2),(7,3,1,2),(8,3,2,2),(9,3,4,2),(10,4,1,2),(11,4,2,2),(12,4,3,2);
 /*!40000 ALTER TABLE `coeficiente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,13 +52,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `mediosdepagos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mediosdepagos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,9 +67,17 @@ CREATE TABLE `mediosdepagos` (
 
 LOCK TABLES `mediosdepagos` WRITE;
 /*!40000 ALTER TABLE `mediosdepagos` DISABLE KEYS */;
-INSERT INTO `mediosdepagos` VALUES (1,'Pesos'),(2,'Cuenta Digital'),(3,'Rapipago'),(4,'Mercadopago'),(5,'Ualá'),(6,'Bitcoin'),(7,'Paypal'),(8,'Payeer');
+INSERT INTO `mediosdepagos` VALUES (1,'Pagofacil'),(2,'Rapipago'),(3,'Bitcoin'),(4,'Paypel');
 /*!40000 ALTER TABLE `mediosdepagos` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'db_saldo'
+--
+
+--
+-- Dumping routines for database 'db_saldo'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -82,4 +88,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-26 21:20:08
+-- Dump completed on 2021-03-01 22:34:56
